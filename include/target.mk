82983ceece36477d26d3614214f8cf84976010db
DingYi <dingyi189@gmail.com>
2024-10-05 14:43:58 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -13,14 +13,14 @@ __target_inc=1
 DEVICE_TYPE?=router
 
 # Default packages - the really basic set
-DEFAULT_PACKAGES:=base-files block-mount busybox dropbear fstools haveged libc libgcc logd mtd netifd opkg uci uclient-fetch uhttpd zoneinfo-asia \
-kmod-fs-ext4 kmod-fs-f2fs kmod-fs-ntfs kmod-fs-vfat kmod-nls-cp936 kmod-usb2 kmod-usb3 kmod-usb-core kmod-usb-storage kmod-scsi-core kmod-tcp-bbr \
-luci-proto-ipv6 luci-proto-ppp luci-mod-admin-full luci-app-ddns luci-app-fileassistant luci-app-firewall luci-app-frpc luci-app-frps \
-luci-app-samba luci-app-ttyd luci-app-wireguard luci-app-wolplus luci-theme-bootstrap
+DEFAULT_PACKAGES:=base-files block-mount busybox dropbear fstools haveged htop libc libgcc logd mtd netifd opkg uci uclient-fetch uhttpd \
+zoneinfo-asia kmod-fs-ext4 kmod-fs-vfat kmod-nls-cp936 kmod-usb2 kmod-usb-core kmod-usb-storage kmod-scsi-core kmod-tcp-bbr luci-proto-ipv6 \
+luci-mod-admin-full luci-app-ddns luci-app-fileassistant luci-app-firewall luci-app-frpc luci-app-frps luci-app-samba luci-app-ttyd luci-app-wireguard \
+luci-app-wolplus luci-theme-bootstrap
 # For nas targets
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
 # For router targets
-DEFAULT_PACKAGES.router:=dnsmasq-full iptables ip6tables ppp ppp-mod-pppoe firewall odhcpd-ipv6only odhcp6c kmod-ipt-offload
+DEFAULT_PACKAGES.router:=dnsmasq-full iptables ip6tables firewall odhcpd-ipv6only odhcp6c kmod-ipt-offload
 DEFAULT_PACKAGES.bootloader:=
 
 ifneq ($(DUMP),)
